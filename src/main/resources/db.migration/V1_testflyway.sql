8896eeb191409dd9ba3f94a1d5b367eb6acdcdc7
hieuhieu112 <hieuahieua@gmail.com>
2026-09-20 17:42:48 +0700

    add db migration

---
+++ src/main/resources/db.migration/V1_testflyway.sql
@@ -0,0 +1,11 @@
+CREATE TABLE abc (
+    column_a VARCHAR(255),
+    column_b VARCHAR(255),
+    column_c VARCHAR(255)
+);
+
+INSERT INTO abc (column_a, column_b, column_c)
+VALUES
+    ('1a', '1b', '1c'),
+    ('2a', '2b', '2c'),
+    ('3a', '3b', '3c');
